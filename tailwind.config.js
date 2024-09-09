84c2de79cfc53685c629cd53a5c2004fd1a910e2
shafiq <shafiqahmad081@gmail.com>
2024-09-09 07:18:20 +0600

    create powerfull section and trusted section and elavate section and bild section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,30 +3,43 @@ module.exports = {
   content: ["./index.html", "./**/*.{js}"],
   theme: {
     fontFamily: {
-      body: ["'Plus Jakarta Sans', sans-serif;"],
-    },
-    container: {
-      center: true,
-      screens: {
-        "3xl": "1920px",
-      },
+      sans: "var(--font-sans)",
     },
 
     extend: {
       colors: {
-        primary: "rgb(var(--color-primary) / <alpha-value>)",
+        background: "rgb(var(--color-primary) / <alpha-value>)",
+        forground: "rgb(var(--color-gray) / <alpha-value>)",
         secondary: "rgb(var(--color-secondary) / <alpha-value>)",
         danger: "rgb(var(--color-danger) / <alpha-value>)",
-        gray: "rgb(var(--color-gray) / <alpha-value>)",
-        gray_primary: "rgb(var(--color-gray-primary) / <alpha-value>)",
-        gray_secondary: "rgb(var(--color-gray-secondary) / <alpha-value>)",
-        card_border: "rgb(var(--color-border) / <alpha-value>)",
+        hover: "rgb(var(--color-hover) / <alpha-value>)",
+        "gray-primary": "rgb(var(--color-gray-primary) / <alpha-value>)",
+        "gray-secondary": "rgb(var(--color-gray-secondary) / <alpha-value>)",
+        "gray-border": "rgb(var(--color-gray-border) / <alpha-value>)",
+        "card-border": "rgb(var(--color-border) / <alpha-value>)",
       },
       fontSize: {
         "7.5xl": "5rem", //80px
+        h1: "96px", // 96px
+        "h1-sm": "48px", // 96px,
+        h2: "3.75rem", // 60px
+        "h2-sm": "2rem", // 32px,
+        h3: "2.5rem", // 40px
+        "h3-sm": "1.25rem", // 20px
+        h4: "2rem", // 32px
+        "h4-sm": "1rem", // 16px
+        h5: "1.25rem", // 20px
       },
       borderRadius: {
         "2lg": "0.675rem",
+      },
+      backgroundImage: {
+        "brand-overlay":
+          "linear-gradient(90deg, #1B1C36 6%, #FFFFFF 54.5%, #191A34 96.3%)",
+      },
+
+      animation: {
+        "spin-slow": "autoRun 10s linear infinite",
       },
     },
   },
